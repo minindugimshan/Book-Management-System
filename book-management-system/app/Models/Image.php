85be7a3d55472897e6f0183262632308ea485a20
minindugimshan2002 <minindugimshan11@gmail.com>
2025-07-23 12:15:05 +0530

    Update Image.php

--- book-management-system/app/Models/Image.php
+++ book-management-system/app/Models/Image.php
@@ -1,10 +1,14 @@
 <?php
+
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['book_id', 'image_path'];
 
     public function book()
