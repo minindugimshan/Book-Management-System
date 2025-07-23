ce400b98ca4e14b7a138945f0c36c34107042d78
minindugimshan2002 <minindugimshan11@gmail.com>
2025-07-23 12:58:22 +0530

    create routes files

--- book-management-system/routes/web.php
+++ book-management-system/routes/web.php
@@ -1,5 +1,33 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\SubcategoryController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\ImageController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\NewPasswordController;
 
-Route::resource('books', BookController::class);
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('categories', CategoryController::class);
+    Route::resource('subcategories', SubcategoryController::class);
+    Route::resource('books', BookController::class);
+    // Route::resource('images', ImageController::class);
+    Route::get('/profile', function () {
+        // You can return a profile edit view here
+        return view('profile.edit');
+    })->name('profile.edit');
+});
+
+Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])
+    ->middleware('guest')
+    ->name('password.reset');
+
+require __DIR__.'/auth.php';
