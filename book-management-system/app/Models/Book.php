73cccedc58a183127b4cf01474745d28a1432c5d
minindugimshan2002 <minindugimshan11@gmail.com>
2025-07-23 12:14:37 +0530

    Add new fields

--- book-management-system/app/Models/Book.php
+++ book-management-system/app/Models/Book.php
@@ -1,14 +1,34 @@
 <?php
+
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    protected $fillable = ['title', 'author', 'publication_year'];
+    use HasFactory;
 
-    public function image()
+    protected $fillable = [
+        'title',
+        'author',
+        'publication_year',
+        'category_id',
+        'subcategory_id',
+    ];
+
+    public function category()
     {
-        return $this->hasOne(Image::class);
+        return $this->belongsTo(Category::class);
+    }
+
+    public function subcategory()
+    {
+        return $this->belongsTo(Subcategory::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(Image::class);
     }
 }
