19a1ccbf0beeb7af2b986fa0bdc09ae125b344a5
minindugimshan2002 <minindugimshan11@gmail.com>
2025-07-23 12:16:42 +0530

    Create RouteServiceProvider.php

---
+++ book-management-system/app/Providers/RouteServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * The path to the "home" route for your application.
+     *
+     * @var string
+     */
+    public const HOME = '/books'; // or '/categories'
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
